3981a086e98c0e4a80a5697a454c4753d9ec014f
ivy <ivy74115@gmail.com>
2023-07-11 01:47:07 +0900

    course 22 commit

--- pinterest/settings.py
+++ pinterest/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os, environ
+from django.urls import reverse_lazy
 
 env = environ.Env(DEBUG=(bool, False))
 
@@ -135,3 +136,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
