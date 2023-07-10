28d1c4ecd06caf6246ebcd6f0e406b8f63ce2b43
ivy <ivy74115@gmail.com>
2023-07-11 03:11:16 +0900

    course 25 commit

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-from accountapp.views import AccountCreateView, AccountDeatailVeiw, hello_world
+from accountapp.views import AccountCreateView, AccountDeatailVeiw, AccountUpdateView, hello_world
 
 app_name = "accountapp"
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('create/', AccountCreateView.as_view(), name='create'),
     path('detail/<int:pk>', AccountDeatailVeiw.as_view(), name='detail'),
+    path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse, HttpResponseRedirect 
+from accountapp.forms import AccountUpdateForm
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView
 from django.views.generic import DetailView
+from django.views.generic import UpdateView
 from accountapp.models import HelloWorld
 
 # Create your views here.
@@ -35,3 +37,10 @@ class AccountDeatailVeiw(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
+
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = AccountUpdateForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/update.html'
