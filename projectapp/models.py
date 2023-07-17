a602d279464440acd166c7f4d9bf2205b1c968f9
ivy <ivy74115@gmail.com>
2023-07-17 19:13:59 +0900

    course 40 commit

---
+++ projectapp/models.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.db import models
+
+# Create your views here.
+class Project(models.Model):
+    image = models.ImageField(upload_to='project/', null=False)
+    title = models.CharField(max_length=20, null=False)
+    description = models.CharField(max_length=200, null=True)
+    created_at = models.DateTimeField(auto_now=True)
---
+++ projectapp/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.urls import reverse
+from projectapp.forms import ProjectCreationForm
+from projectapp.models import Project
+from django.views.generic import CreateView, DetailView, ListView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
+
+
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
+class ProjectCreateView(CreateView):
+    model = Project
+    form_class = ProjectCreationForm
+    template_name = 'projectapp/create.html'
+
+    def get_success_url(self):
+        return reverse('projectapp:detail', kwargs={'pk':self.object.pk})
+    
+
+class ProjectDetailView(DetailView):
+    model = Project
+    context_object_name = 'target_project'
+    template_name = 'projectapp/detail.html'
+
+
+class ProjectListView(ListView):
+    model = Project
+    context_object_name = 'project_list'
+    template_name = 'projectapp/list.html'
+    paginate_by = 25
+
+    
+    
