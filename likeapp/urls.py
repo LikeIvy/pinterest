9c02f487f58bf1843eff7fe7f2ab51709cfa35a1
ivy <ivy74115@gmail.com>
2023-08-04 17:29:53 +0900

    add like funtion

---
+++ likeapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from likeapp.views import LikeArticleView
+
+
+
+app_name = 'likeapp'
+
+
+
+urlpatterns = [
+    path('article/like/<int:pk>', LikeArticleView.as_view(), name='article_like'),
+]
